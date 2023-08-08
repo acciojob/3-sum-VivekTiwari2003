408b370ff0668749778eaa3101171e421e55988e
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-08-08 18:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function threeSum(arr, target) {
 
-	for(let i = 0;i<arr.length;i++){
-		if(arr[i]+arr[i+1]+arr[i+2]== target){
-			
-		}
-	}
-}
-
+	  let closestSum = Infinity;
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            for (let k = j + 1; k < arr.length; k++) {
+                let sum = arr[i] + arr[j] + arr[k];
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+            }
+        }
+    }
+    
+    return closestSum;
 module.exports = threeSum;
